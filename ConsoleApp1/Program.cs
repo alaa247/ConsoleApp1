855508603710735d7f038fcbdec19ef450d49483
alaa alahmad <alahmedalaa@gmail.com>
2019-10-21 12:15:10 +0200

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine ("welcome to lexicon" );
+            Console.WriteLine("how are you");
+            Console.WriteLine("what do you do");
+        }
+    }
+}
